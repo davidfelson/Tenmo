9e60e58c9a67059ef9490c35b17f036be15c10b9
Richard Millard <xrichmillx@gmail.com>
2020-11-04 15:03:56 -0500

    added account interface and account controller

--- 18_Capstone/TenmoServer/Controllers/AccountController.cs
+++ 18_Capstone/TenmoServer/Controllers/AccountController.cs
@@ -2,14 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TenmoClient.Data;
+using TenmoServer.DAO;
 
 namespace TenmoServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
+    [Authorize]
     public class AccountController : ControllerBase
     {
+        private IAccountsDAO accntDAO;
+
+        public AccountController(IAccountsDAO accountDAO)
+        {
+            this.accntDAO = accountDAO;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Accounts> GetAccountBalance(int id)
+        {
+            Accounts account = accntDAO.GetAccountBalance(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return account;
+                    
+            }
+
+        }
+
+
     }
 }
